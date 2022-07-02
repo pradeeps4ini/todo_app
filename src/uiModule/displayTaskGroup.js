cfef0d29798d83fd1d17cb461a24081c0d0e1bd6
pradeeps4ini <pradeepsaini@protonmail.com>
2022-07-02 17:02:31 +0530

    restarting the project

--- src/uiModule/displayTaskGroup.js
+++
@@ -1,8 +0,0 @@
-const displayGroup = function(groupName) {
-  const TaskGroups = document.querySelector(".task-groups");
-  const p = document.createElement("p");
-  p.textContent = groupName;
-  TaskGroups.appendChild(p);
-};
-
-export default displayGroup;
